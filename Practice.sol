cfd0d7736621e194a5c473d576d5d09c9a70b058
Chaeguevara <dfso2@naver.com>
2022-01-20 22:11:38 +0900

    Part1 WIP

---
+++ Practice.sol
@@ -0,0 +1,22 @@
+//Klaytn IDE uses solidity 0.4.24, 0.5.6 versions.
+pragma solidity >=0.4.24 <=0.5.6;
+
+contract Practice{
+    uint256 private totalSupply = 10 ;
+    string public name = "KlayLion";
+
+    address public owner; //contract deployer
+    mapping(uint256 => string) public tokenURIs;
+
+    constructor () public {
+        owner = msg.sender;
+    }
+    function getTotalSupply() public view returns (uint256){
+        return totalSupply * 1000000;
+    }
+
+    function setTotalSupply(uint256 newSupply) public{
+        require(owner == msg.sender, 'Not owner');
+        totalSupply = newSupply;
+    }
+}
